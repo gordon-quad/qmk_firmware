945d2a559f578ff759bb5669b41f2051f9aad463
Gordon Quad <gordon@niflheim.info>
2017-04-26 09:41:05 +0100

    yoba layout refined

--- keyboards/lets_split/keymaps/i2c-yoba/keymap.c
+++ keyboards/lets_split/keymaps/i2c-yoba/keymap.c
@@ -5,6 +5,9 @@
 #define LOWER 1
 #define RAISE 2
 
+#define TO_LWR MO(LOWER)
+#define TO_RSE MO(RAISE)
+
 // Alias to make layering more clear
 #define _______ KC_TRNS
 
@@ -16,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctl  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Sh/Ent|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  | Del  | Alt  | GUI  |Lower | Bksp |Space |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
@@ -25,43 +28,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_CAPS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT), \
-    KC_ESC,  KC_DEL,  KC_LALT, KC_LGUI, MO(1),   KC_BSPC, KC_SPC,  MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_ESC,  KC_DEL,  KC_LALT, KC_LGUI, TO_LWR,  KC_BSPC, KC_SPC,  TO_RSE,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |  |   |   &  |   *  |   (  |   )  | Del  |
+ * |      | F1   | F2   | F3   | F4   | Vol+ |  |   |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      | Mute | Vol- | Vol+ |      | Bksp |   $  |   %  |   ^  |   [  |  ]   |
+ * |      | F5   | F6   | F7   | F8   | Vol- | Bksp |   $  |   %  |   ^  |   [  |  ]   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   !  |   @  |   #  |   \  |Enter |
+ * |      | F9   | F10  | F11  | F12  | Mute |      |   !  |   @  |   #  |   \  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Del  |Enter |      |      | PgDn | PgUp |      |
+ * |      |      |      |      |      | Del  |Enter |      | Home | PgDn | PgUp |End   |
  * `-----------------------------------------------------------------------------------'
  */
 [LOWER] = KEYMAP( \
-  _______, _______, _______, _______, _______, _______, KC_PIPE, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, \
-  _______, _______, _______, _______, _______, _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, MT(MOD_RSFT, KC_ENT), \
-  _______, _______, _______, _______, _______, KC_DEL,  KC_ENT,  _______, _______, KC_PGDN, KC_PGUP, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_VOLU, KC_PIPE, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_VOLD, KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, \
+  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MUTE, _______, KC_EXLM, KC_AT,   KC_HASH, KC_BSLS, _______, \
+  _______, _______, _______, _______, _______, KC_DEL,  KC_ENT,  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   `  |   ~  |   (  |   )  |      |      |   7  |   8  |   9  |      |      |
+ * |   `  |   `  |   ~  |   (  |   )  |      |  |   |   7  |   8  |   9  |  -   |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  [   |   ]  |   {  |   }  |      |      |   4  |   5  |   6  |      |  \   |
+ * |      |  [   |   ]  |   {  |   }  |      |Bksp  |   4  |   5  |   6  |  +   |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  -   |   _  |  =   |  +   |      |      |   1  |   2  |   3  |      |Enter |
+ * |      |  -   |   _  |  =   |  +   |      |      |   1  |   2  |   3  |  *   |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |    Enter    |   0  |   .  | PgDn | PgUp |      |
+ * |      |      |      |      |      |Del   |Enter |   0  |   .  | PgDn | PgUp |      |
  * `-----------------------------------------------------------------------------------'
  */
 
 [RAISE] = KEYMAP( \
-  KC_GRV,  KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, \
-  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, _______, KC_4,    KC_5,    KC_6,    _______, KC_BSLS, \
-  _______, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______, _______, KC_1,    KC_2,    KC_3,    _______, KC_ENT, \
-  _______, _______, _______, _______, _______, KC_ENT,  KC_ENT,  KC_0,    KC_DOT,  KC_PGDN, KC_PGUP, _______ \
+  KC_GRV,  KC_GRV,  KC_TILD, KC_LPRN, KC_RPRN, _______, KC_PIPE, KC_7,    KC_8,    KC_9,    KC_MINS, KC_SLSH, \
+  _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______, KC_BSPC, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_BSLS, \
+  _______, KC_MINS, KC_UNDS, KC_EQL,  KC_PLUS, _______, _______, KC_1,    KC_2,    KC_3,    KC_ASTR, _______, \
+  _______, _______, _______, _______, _______, KC_DEL,  KC_ENT,  KC_0,    KC_DOT,  KC_PGDN, KC_PGUP, _______ \
 )
 };
